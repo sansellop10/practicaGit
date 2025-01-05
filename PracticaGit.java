b3c26b5a0ec28e4a104b1213eb436e8827a90cc3
Santi <santiselva2022@gmail.com>
2025-01-05 19:46:16 +0100

    Conversor a binario

--- PracticaGit.java
+++ PracticaGit.java
@@ -5,5 +5,18 @@ public class PracticaGit{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         ArrayList <Integer> lista = new ArrayList<>();
+        
+        System.out.print("Introduce numero en decimal para convertirlo a binario: ");
+        int numero = in.nextInt();
+        int num = numero;
+        do {
+            lista.add(num % 2);
+            num/=2;
+        } while (num>0);
+
+        System.out.print("El numero "+numero+" en binario es: ");
+        for(int i = (lista.size()-1); i>=0; i--){
+            System.out.print(lista.get(i));
+        }
     }
 }
