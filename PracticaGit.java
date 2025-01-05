66950eb185434355f555686eb90623d894ccb7cb
Santi <santiselva2022@gmail.com>
2025-01-05 19:31:21 +0100

    Plantilla inicial

---
+++ PracticaGit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.sql.Array;
+import java.util.ArrayList;
+public class PracticaGit{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        ArrayList <Integer> lista = new ArrayList<>();
+    }
+}
